112f5d95aad85160b6373ebd259abcdaca66a972
Xavier Lupin <46515511+xavierlupin@users.noreply.github.com>
2022-06-06 14:06:50 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+const srcPath = path.resolve(__dirname, 'src')
+const distPath = path.resolve(__dirname, 'docs')
+
+module.exports = {
+  entry: path.join(srcPath, 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(distPath, 'js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+}
