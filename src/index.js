ef05da5325bd940783e22e5a67a7a7ca7fa6c453
Xavier Lupin <xavierlupin0418@gmail.com>
2022-06-06 16:23:22 +0800

    feat(src): add promiseMachine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-window.addEventListener('DOMContentLoaded', () => {})
+import { interpret } from 'xstate'
+import promiseMachine from './machines/promiseMachine'
+
+const DOM = {
+  promise: {
+    stateInput: null,
+    resolveBtn: null,
+    rejectBtn: null,
+  },
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  DOM.promise.stateInput = document.getElementById('promiseState')
+  DOM.promise.resolveBtn = document.getElementById('promiseResolveBtn')
+  DOM.promise.rejectBtn = document.getElementById('promiseRejectBtn')
+
+  const promiseService = interpret(promiseMachine).onTransition((state) => {
+    DOM.promise.stateInput.value = state.value
+  })
+
+  DOM.promise.resolveBtn.addEventListener('click', (e) => {
+    promiseService.send({ type: 'RESOLVE' })
+  })
+
+  DOM.promise.rejectBtn.addEventListener('click', (e) => {
+    promiseService.send({ type: 'REJECT' })
+  })
+
+  promiseService.start('pending')
+})
